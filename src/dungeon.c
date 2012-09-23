b3f7de299d5001b722e400a32e963513c3f9973c
nppangband <nppangbanddev@verizon.net>
2012-09-23 16:58:46 -0400

    Fix a problem with object generation on arena levels.
    Signed-off-by: nppangband <nppangbanddev@verizon.net>

--- src/dungeon.c
+++ src/dungeon.c
@@ -535,6 +535,9 @@ static s16b get_arena_obj_num(void)
 			else if (level <= 75) 	sval = SV_AMMO_NORMAL;
 			else 					sval = SV_AMMO_HEAVY;
 
+			/* Hack - the svals for shots are slightly different than bows and bolts */
+			if ((tval == TV_ARROW || tval == TV_BOLT) sval++;
+
 			k_idx = lookup_kind(tval, sval);
 		}
 	}
