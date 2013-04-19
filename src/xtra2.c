bcf09220c9ca3c0afd88dcb8566c6c407021b1e0
nppangband <nppangbanddev@verizon.net>
2013-04-19 17:42:10 -0400

    Bugfix:  Paste error while importing moria code.
    Signed-off-by: nppangband <nppangbanddev@verizon.net>

--- src/xtra2.c
+++ src/xtra2.c
@@ -760,7 +760,7 @@ static s32b calc_mon_exp(const monster_race *r_ptr)
 		 * Player is not gaining a new max level
 		 * (in the player_exp chart level 1 exp-to-gain-next-level is at slot 0)
 		 */
-		if ((p_ptr->exp + new_exp) <= (get_experience_by_level(new_level-1))) break;
+		if ((p_ptr->exp + new_exp) <= (get_experience_by_level(new_level-1)) * p_ptr->expfact / 100L) break;
 
 		/*just checking this again*/
 		if (new_exp < 1) break;
