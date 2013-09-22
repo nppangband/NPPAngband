c60db878205d66543ef26926718c253f89f60a93
dlevin <levin376@gmail.com>
2013-09-22 02:15:57 -0400

    Add angband-style header comment to z-form.h
    Mostly copied from z-form.c

--- src/z-form.h
+++ src/z-form.h
@@ -1,3 +1,20 @@
+/*
+ * File: z-form.h
+ * Purpose: Low-level text formatting (snprintf() replacement)
+ *
+ * Copyright (c) 1997 Ben Harrison
+ *
+ * This work is free software; you can redistribute it and/or modify it
+ * under the terms of either:
+ *
+ * a) the GNU General Public License as published by the Free Software
+ *    Foundation, version 2, or
+ *
+ * b) the "Angband licence":
+ *    This software may be copied and distributed for educational, research,
+ *    and not for profit purposes provided that this copyright and statement
+ *    are included in all such copies.  Other copyrights may also apply.
+ */
 #ifndef INCLUDED_Z_FORM_H
 #define INCLUDED_Z_FORM_H
 
